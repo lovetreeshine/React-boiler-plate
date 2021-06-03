cd897cd80003802ff95bfa15350609561d7ccdf8
Jin <lovetreeshine@gmail.com>
2021-06-03 15:30:07 +0900

    비밀정보 보호

--- index.js
+++ index.js
@@ -2,10 +2,23 @@ const express = require('express')
 const app = express()
 const port = 5002
 
+const bodyParser = require('body-parser');
+
+const config = require('./config/key')
+const { User } = require("./models/User");
+
+//application/x-www-form-urlencoded
+// app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.urlencoded({extended: true}));
+
+//application/json
+// app.use(bodyParser.json());
+app.use(express.json());
 
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://zinkang:abcd1234@boilerplate.fcut2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+// mongoose.connect('mongodb+srv://zinkang:abcd1234@boilerplate.fcut2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
@@ -17,6 +30,18 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+app.post('/register', (req, res) => {
+
+    // 회원 가입 할 때 필요한 정보들을 client에서 가져오면
+    // 그것들을 데이터 베이스에 넣어준다.
+    const user = new User(req.body)
+
+    user.save((err, userInfo) => {
+      if (err) return res.json({success: false, err})
+      return res.status(200).json({
+        success: true
+      })
+    })
 })
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
